fa0a77889ad57bdd2211e8884ab0564b441f208d
Jordan <101655927+jordieshore@users.noreply.github.com>
2022-08-24 17:41:53 +0200

    Add files via upload

---
+++ Token/token.sol.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+//Impport ERC20 contract from OppenZeplin
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract SquareToken is ERC20 {
+
+    //We also want to call the constructor present inside ERC 20
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 10*(10**18));
+    }
+
+    function mint() public{
+         _mint(msg.sender, 10*(10**18));
+    }
+
+}
